28cd4091251539d45ea51e9f99e4a0fe2bed2af9
Rezaei Rahmat <2249301106@qq.com>
2018-01-30 13:12:41 +0900

    feat: validate input

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.onChange = this.onChange.bind(this)
+  }
 
-
+  onChange(event) {
+    const inputNumber = /^[1-9]+$/;
+    if (event.target.value == "" || inputNumber.test(event.target.value)) {
+      this.setState({value: event.target.value})
+    }
+  }
   render() {
-    const element = React.createElement (
-      'input',
-      {type: 'number'}
-    )
+
     return (
-      <form>{element}</form>
+      <form>
+        <input value={this.state.value} onChange={this.onChange} maxLength="1" />
+      </form>
     );
   }
 }
