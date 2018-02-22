f081ee0feda90bd90c63adf24f13ad7fe75592b0
Rezaei Rahmat <2249301106@qq.com>
2018-02-22 14:15:27 +0900

    feat: add mousemove event

--- src/Component/temperature-chart.js
+++ src/Component/temperature-chart.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import { XYPlot, LineSeries } from 'react-vis';
+import { XYPlot, LineSeries, VerticalBarSeries, MarkSeries } from 'react-vis';
 
 class Chart extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: [
+          {x: 0, y: 8},
+          {x: 1, y: 5},
+        ]
+      }
+      let i =0;
+      window.addEventListener("mousemove",({clientX, clientY}) => {
+        this.setState({
+          data: this.state.data.concat({
+            x: i ,
+             y : clientY % 10
+           })
+         })
+         i++;
+      })
+    }
   render() {
-    const data = [
-      {x: 0, y: 8},
-      {x: 1, y: 5},
-      {x: 2, y: 4},
-      {x: 3, y: 9},
-      {x: 4, y: 1},
-      {x: 5, y: 7},
-      {x: 6, y: 6},
-      {x: 7, y: 3},
-      {x: 8, y: 2},
-      {x: 9, y: 0}
-    ];
+    const { data } = this.state;
     return (
       <div className="App">
-        <XYPlot height={300} width={300}>
-          <LineSeries data={data} />
-        </XYPlot>
+      <XYPlot height={800} width={1800}>
+        <VerticalBarSeries data={data} />
+      </XYPlot>
+      <XYPlot height={200} width={200}>
+        <LineSeries data={data} />
+      </XYPlot>
+      <XYPlot height={200} width={200}>
+        <MarkSeries data={data} />
+      </XYPlot>
       </div>
     );
   }
